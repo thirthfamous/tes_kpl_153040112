f4dc5cc71cd12368f26e0be89ab156c57ce6aeb1
thirthfamous <fhbluebeast356@gmail.com>
2018-12-29 07:34:47 +0700

    commit pertama

---
+++ src/main/java/src/main/java/com/farhanhamdallah/farhanhamdallah_153040112/A.java
@@ -0,0 +1,10 @@
+package src.main.java.com.farhanhamdallah.farhanhamdallah_153040112;
+
+public class A {
+	public double hitung(double a, double b, double c) {
+		Argument argA = new Argument("a", a);
+		Argument argB = new Argument("a", b);
+		Argument argC = new Argument("a", c);
+		Expression exp = new Expression("(a+b)*c", argB, argA, argC);
+	}
+}
