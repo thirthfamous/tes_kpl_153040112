a657c86f628df5f05c6717a426ffab6b3ddbecc6
thirthfamous <fhbluebeast356@gmail.com>
2018-12-29 08:15:27 +0700

    push ke 2

--- src/main/java/src/main/java/com/farhanhamdallah/farhanhamdallah_153040112/A.java
+++ src/main/java/src/main/java/com/farhanhamdallah/farhanhamdallah_153040112/A.java
@@ -1,11 +1,14 @@
 package src.main.java.com.farhanhamdallah.farhanhamdallah_153040112;
 
+import org.mariuszgromada.math.mxparser.Argument;
+import org.mariuszgromada.math.mxparser.Expression;
+
 public class A {
 	public double hitung(double a, double b, double c) {
-		Argument argA = new Argument("a", a);
-		Argument argB = new Argument("a", b);
-		Argument argC = new Argument("a", c);
-		Expression exp = new Expression("(a+b)*c", argB, argA, argC);
+		org.mariuszgromada.math.mxparser.Argument argA = new Argument("a", a);
+		Argument argB = new Argument("b", b);
+		Argument argC = new Argument("c", c);
+		org.mariuszgromada.math.mxparser.Expression exp = new Expression("(a+b)*c", argB, argA, argC);
 		return exp.calculate();
 	}
 }
